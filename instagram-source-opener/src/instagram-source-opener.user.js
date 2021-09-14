8855c6e2bf08c5431575c0fb23d2b34de569af90
José Pereira <jmfp-b@live.com.pt>
2021-09-14 22:02:31 +0100

    chore(versioning): bump iso patch version

--- instagram-source-opener/src/instagram-source-opener.user.js
+++ instagram-source-opener/src/instagram-source-opener.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name             Instagram Source Opener
-// @version          1.1.20
+// @version          1.1.21
 // @description      Open the original source of an IG post, story or profile picture
 // @author           jomifepe
 // @license          MIT
